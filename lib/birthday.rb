ed81f005b33621f3e984b06d158f351a59717079
Ad Friedman <adien330@yahoo.com>
2019-03-22 18:09:39 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-birthday_kids.each do |i,v|
-  puts "Happy Birthday #{i}! You are now #{v} years old!"
+  birthday_kids.each do |i,v|
+    puts "Happy Birthday #{i}! You are now #{v} years old!"
+  end
 end
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,8 +6,12 @@ def select_winner(passengers)
   suite_d: "Charlie Chaplin",
   suite_e: "Crumpet the Elf"
   }
-
-
-
+g = ""
+  passengers.each do |i,v|
+        if i == :suite_a and v[0] == "A"
+          g = v
+        end
+  end
+g
 
 end
